8772d4b00008a1a3647589c1c6a2227f38cf05ef
reganoneill <rkrotho@gmail.com>
2016-11-08 10:35:46 -0800

    client side routing and menu work

--- scripts/controller/aboutController.js
+++ scripts/controller/aboutController.js
@@ -3,8 +3,8 @@
 
   aboutController.reveal = function() {
     /* TODO: Use your DOM skills to reveal only the about section! */
-    //MAKE SURE THIS WORKS...IT DOESN'T RN
-    $('#articles').hide();
+    //MAKE SURE THIS WORKS...
+    $('#home').hide();
     $('#about').show();
   };
 
